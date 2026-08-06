7d34401b4dec448d49f9c2863780d1f70bbc3a02
Shashi Shekhar Pandey <78402453+CodewithShashi@users.noreply.github.com>
2026-08-06 20:23:44 +0530

    Update resume.tsx

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -8,7 +8,7 @@ export const DATA = {
   location: "Noida, India",
   locationLink: "",
   description:
-    "I'm software Engineer from India with expertise in UI development and UI/UX technologies for user interface development. staying ahead of technological trends by constantly investigating new tools.",
+    "I'm a Software Engineer from India with expertise in Web, UI Development, and UI/UX Design, creating intuitive, user-centered digital experiences. Passionate about innovation, I stay ahead of technological trends by continuously exploring emerging tools, AI technologies, and modern development practices to build smarter, scalable, and impactful digital solutions.",
   summary:
     "UI Developer, crafting responsive, user-centric websites. With 3 years of experience, I deliver high-performance, cross-browser compatible solutions. Passionate about enhancing user experiences through innovative design and performance optimization, while staying updated with modern web trends.",
   avatarUrl: "/Profile.png",
